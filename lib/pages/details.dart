a5ebe1a4e663ae9fc6901d2068f50ee5d382e925
Sky Yong <sky.yong@websoft.sg>
2025-06-06 17:35:16 +0800

    part 2 completed

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fooddeliveryapp/widget/widget_support.dart';
 
 class Details extends StatefulWidget {
   const Details({super.key});
@@ -8,11 +9,144 @@ class Details extends StatefulWidget {
 }
 
 class _DetailsState extends State<Details> {
+  int a = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Column(),
+        margin: EdgeInsets.only(top: 50, left: 20, right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Icon(
+                Icons.arrow_back_ios_new_outlined,
+                color: Colors.black,
+              ),
+            ),
+            Image.asset(
+              "images/salad2.png",
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.5,
+              fit: BoxFit.fill,
+            ),
+
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Mediterranean Salad",
+                      style: AppWidget.semiBoldTextFeildStyle(),
+                    ),
+                    Text("chicke Salad", style: AppWidget.boldTextFeildStyle()),
+                  ],
+                ),
+                Spacer(),
+                GestureDetector(
+                  onTap: () => {if (a > 1) --a, setState(() {})},
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Icon(Icons.remove, color: Colors.white),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Text(a.toString(), style: AppWidget.semiBoldTextFeildStyle()),
+                SizedBox(width: 20),
+                GestureDetector(
+                  onTap: () => {a = a + 1, setState(() {})},
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Icon(Icons.add, color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+              maxLines: 3,
+              style: AppWidget.lightTextFeildStyle(),
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: [
+                Text(
+                  "Delivery Time",
+                  style: AppWidget.semiBoldTextFeildStyle(),
+                ),
+                SizedBox(width: 25.0),
+                Icon(Icons.alarm, color: Colors.black54),
+                SizedBox(width: 5.0),
+                Text("30 mins", style: AppWidget.semiBoldTextFeildStyle()),
+              ],
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 40.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Total Price",
+                        style: AppWidget.semiBoldTextFeildStyle(),
+                      ),
+                      Text("\$28", style: AppWidget.headerlineTextFeildStyle()),
+                    ],
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 2,
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "Add to cart",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                            fontFamily: 'Rubik',
+                          ),
+                        ),
+                        SizedBox(width: 30),
+                        Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Icon(
+                            Icons.shopping_cart_outlined,
+                            color: Colors.white,
+                            size: 20.0,
+                          ),
+                        ),
+                        SizedBox(width: 10,)
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fooddeliveryapp/pages/details.dart';
 import 'package:fooddeliveryapp/widget/widget_support.dart';
 
 class Home extends StatefulWidget {
@@ -46,37 +47,45 @@ class _HomeState extends State<Home> {
               scrollDirection: Axis.horizontal,
               child: Row(
                 children: [
-                  Container(
-                    margin: EdgeInsets.all(4),
-                    child: Material(
-                      elevation: 5.0,
-                      borderRadius: BorderRadius.circular(20),
-                      child: Container(
-                        padding: EdgeInsets.all(14),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Image.asset(
-                              "images/salad2.png",
-                              height: 150,
-                              width: 150,
-                              fit: BoxFit.cover,
-                            ),
-                            Text(
-                              "Veggie Taco hash",
-                              style: AppWidget.semiBoldTextFeildStyle(),
-                            ),
-                            SizedBox(height: 5.0),
-                            Text(
-                              "Fresh and Healty",
-                              style: AppWidget.lightTextFeildStyle(),
-                            ),
-                            SizedBox(height: 5.0),
-                            Text(
-                              "\$25",
-                              style: AppWidget.semiBoldTextFeildStyle(),
-                            ),
-                          ],
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Details()),
+                      );
+                    },
+                    child: Container(
+                      margin: EdgeInsets.all(4),
+                      child: Material(
+                        elevation: 5.0,
+                        borderRadius: BorderRadius.circular(20),
+                        child: Container(
+                          padding: EdgeInsets.all(14),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                "images/salad2.png",
+                                height: 150,
+                                width: 150,
+                                fit: BoxFit.cover,
+                              ),
+                              Text(
+                                "Veggie Taco hash",
+                                style: AppWidget.semiBoldTextFeildStyle(),
+                              ),
+                              SizedBox(height: 5.0),
+                              Text(
+                                "Fresh and Healty",
+                                style: AppWidget.lightTextFeildStyle(),
+                              ),
+                              SizedBox(height: 5.0),
+                              Text(
+                                "\$25",
+                                style: AppWidget.semiBoldTextFeildStyle(),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
