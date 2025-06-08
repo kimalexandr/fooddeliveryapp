c3a043054c79585b3fa1f9d01e59b456329380eb
Sky Yong <jinghao0958@email.com>
2025-06-08 23:26:28 +0800

    part 3 beginning

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fooddeliveryapp/pages/bottomnav.dart';
 import 'package:fooddeliveryapp/pages/home.dart';
+import 'package:fooddeliveryapp/pages/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: BottomNav(),
+      home: LogIn(),
     );
   }
 }
